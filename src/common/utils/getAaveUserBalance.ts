1908dc4a0679e17404e71724c98157682a7e80ae
Billy19191 <bill.ratchanon@gmail.com>
2024-12-27 23:45:10 +0700

    feat: update Aave user balance fetching to support BNB network

--- src/common/utils/getAaveUserBalance.ts
+++ src/common/utils/getAaveUserBalance.ts
@@ -20,7 +20,7 @@ import { formatReserves, formatReservesAndIncentives } from '@aave/math-utils'
 
 // Sample RPC address for querying ETH mainnet
 const provider = new ethers.providers.JsonRpcProvider(
-  'https://op-pokt.nodies.app'
+  'https://binance.llamarpc.com'
 )
 
 // User address to fetch data for, insert address here
@@ -29,16 +29,15 @@ const currentAccount = '0x6FdfD0Dee3e2aa16bA6c70B1398E640735D5C065'
 // View contract used to fetch all reserves data (including market base currency data), and user reserves
 // Using Aave V3 Eth Mainnet address for demo
 const poolDataProviderContract = new UiPoolDataProvider({
-  uiPoolDataProviderAddress: markets.AaveV3Optimism.UI_POOL_DATA_PROVIDER,
+  uiPoolDataProviderAddress: markets.AaveV3BNB.UI_POOL_DATA_PROVIDER,
   provider,
-  chainId: ChainId.optimism,
+  chainId: ChainId.bnb,
 })
 
 // View contract used to fetch all reserve incentives (APRs), and user incentives
 // Using Aave V3 Eth Mainnet address for demo
 const incentiveDataProviderContract = new UiIncentiveDataProvider({
-  uiIncentiveDataProviderAddress:
-    markets.AaveV3Optimism.UI_INCENTIVE_DATA_PROVIDER,
+  uiIncentiveDataProviderAddress: markets.AaveV3BNB.UI_INCENTIVE_DATA_PROVIDER,
   provider,
   chainId: ChainId.optimism,
 })
@@ -47,28 +46,26 @@ async function fetchContractData() {
   // Object containing array of pool reserves and market base currency data
   // { reservesArray, baseCurrencyData }
   const reserves = await poolDataProviderContract.getReservesHumanized({
-    lendingPoolAddressProvider: markets.AaveV3Optimism.POOL_ADDRESSES_PROVIDER,
+    lendingPoolAddressProvider: markets.AaveV3BNB.POOL_ADDRESSES_PROVIDER,
   })
 
   // Object containing array or users aave positions and active eMode category
   // { userReserves, userEmodeCategoryId }
   const userReserves = await poolDataProviderContract.getUserReservesHumanized({
-    lendingPoolAddressProvider: markets.AaveV3Optimism.POOL_ADDRESSES_PROVIDER,
+    lendingPoolAddressProvider: markets.AaveV3BNB.POOL_ADDRESSES_PROVIDER,
     user: currentAccount,
   })
 
   // Array of incentive tokens with price feed and emission APR
   const reserveIncentives =
     await incentiveDataProviderContract.getReservesIncentivesDataHumanized({
-      lendingPoolAddressProvider:
-        markets.AaveV3Optimism.POOL_ADDRESSES_PROVIDER,
+      lendingPoolAddressProvider: markets.AaveV3BNB.POOL_ADDRESSES_PROVIDER,
     })
 
   // Dictionary of claimable user incentives
   const userIncentives =
     await incentiveDataProviderContract.getUserReservesIncentivesDataHumanized({
-      lendingPoolAddressProvider:
-        markets.AaveV3Optimism.POOL_ADDRESSES_PROVIDER,
+      lendingPoolAddressProvider: markets.AaveV3BNB.POOL_ADDRESSES_PROVIDER,
       user: currentAccount,
     })
 
@@ -76,7 +73,7 @@ async function fetchContractData() {
   const baseCurrencyData = reserves.baseCurrencyData
   const userReservesArray = userReserves.userReserves
 
-  const currentTimestamp = dayjs().subtract(10, 'days').unix()
+  const currentTimestamp = dayjs().unix()
 
   // const formattedPoolReserves = formatReserves({
   //   reserves: reservesArray,
